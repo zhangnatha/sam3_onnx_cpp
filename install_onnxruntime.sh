632e26cde607d821bb12e0b7b9ffd0686c1a54e8
zhangjianan <zhangnatha@msn.cn>
2026-01-14 13:31:24 +0800

    update

--- install_onnxruntime.sh
+++ install_onnxruntime.sh
@@ -5,8 +5,8 @@ set -e
 
 # 定义安装路径
 SOURCE_DIR="$(pwd)"
-INSTALL_GPU_DIR="$(pwd)/3rdpartyU18/onnxruntime-cuda12-1.17.3"
-INSTALL_CPU_DIR="$(pwd)/3rdpartyU18/onnxruntime"
+INSTALL_GPU_DIR="$(pwd)/3rdparty/onnxruntime-gpu"
+INSTALL_CPU_DIR="$(pwd)/3rdparty/onnxruntime-cpu"
 
 # 1. 下载onnxruntime CPU/GPU
 echo "下载onnxruntime lib..."
@@ -29,5 +29,7 @@ mv "$SOURCE_DIR/onnxruntime-linux-x64-1.17.3"/* "$INSTALL_CPU_DIR/"
 
 # 3. 清理下载的压缩文件
 echo "清除..."
-rm onnxruntime-linux-x64-gpu-cuda12-1.17.3.tgz onnxruntime-linux-x64-gpu-1.17.3
-rm onnxruntime-linux-x64-1.17.3.tgz onnxruntime-linux-x64-1.17.3
+rm onnxruntime-linux-x64-gpu-cuda12-1.17.3.tgz 
+rm -rf onnxruntime-linux-x64-gpu-1.17.3
+rm onnxruntime-linux-x64-1.17.3.tgz
+rm -rf onnxruntime-linux-x64-1.17.3
